5bbf1b589d290da3e30f3b2f63fc0b9670ec3e7e
Velho-do-Mal <marcio@bk-engenharia.com>
2026-03-27 14:58:00 -0300

    Update apps.py

--- apps/orcamento/apps.py
+++ apps/orcamento/apps.py
@@ -1,1 +1,8 @@
-#
+from django.apps import AppConfig
+
+
+class OrcamentoConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.orcamento"
+    verbose_name = "Orçamento"
+
