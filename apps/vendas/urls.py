d9312b3f0ff26681bd242b5b6a87bb7168fe65af
Velho-do-Mal <marcio@bk-engenharia.com>
2026-05-27 09:15:58 -0300

    feat: vendas - adiciona rotas para nova proposta, detalhe e exportar Word

--- apps/vendas/urls.py
+++ apps/vendas/urls.py
@@ -5,4 +5,7 @@ app_name = 'vendas'
 
 urlpatterns = [
     path('vendas/', views.lista, name='lista'),
+    path('vendas/propostas/nova/', views.proposta_nova, name='proposta_nova'),
+    path('vendas/propostas/<int:pk>/', views.proposta_detalhe, name='proposta_detalhe'),
+    path('vendas/propostas/<int:pk>/exportar-word/', views.exportar_word, name='exportar_word'),
 ]
